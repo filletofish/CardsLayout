02dd05e41fb25c3e3f9699e42de3f595459b5f78
Filipp Fediakov <julik103@mail.ru>
2017-11-03 11:32:25 +0300

    Change example color, appearance

--- CardsExample/CardsExample/ViewController.swift
+++ CardsExample/CardsExample/ViewController.swift
@@ -21,17 +21,32 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     collectionView.showsHorizontalScrollIndicator = false
   }
 
-  var colors: [UIColor]  = [.red, .green, .yellow, .blue, .orange, .black, .gray]
+  var colors: [UIColor]  = [
+    UIColor(red: 237, green: 37, blue: 78),
+    UIColor(red: 249, green: 220, blue: 92),
+    UIColor(red: 194, green: 234, blue: 189),
+    UIColor(red: 1, green: 25, blue: 54),
+    UIColor(red: 255, green: 184, blue: 209)
+  ]
 
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCellReuseIdentifier", for: indexPath)
-    cell.layer.cornerRadius = 5.0
+    cell.layer.cornerRadius = 7.0
     cell.backgroundColor = colors[indexPath.row]
     return cell
   }
 
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return colors.count
+  }
+}
+
+extension UIColor {
+  convenience init(red: Int, green: Int, blue: Int) {
+    self.init(red: CGFloat(red)/255 ,
+              green: CGFloat(green)/255,
+              blue: CGFloat(blue)/255,
+              alpha: 1.0)
   }
 }
 
